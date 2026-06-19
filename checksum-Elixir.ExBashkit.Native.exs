ae7d0ecf9eee2c8731530651fb474063528cf87e
James Tippett <james.tippett@gmail.com>
2026-06-19 20:27:54 +0700

    Add precompiled NIF checksums for v0.1.0

---
+++ checksum-Elixir.ExBashkit.Native.exs
@@ -0,0 +1,6 @@
+%{
+  "libex_bashkit-v0.1.0-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:4613586a13bac279ff12d42f08a129f535a1de417a74a03b65af3b365c720e49",
+  "libex_bashkit-v0.1.0-nif-2.15-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:0efe59b01bcce706def6fc7d5c155db9072937c779a1655de193ca663917ff96",
+  "libex_bashkit-v0.1.0-nif-2.15-x86_64-apple-darwin.so.tar.gz" => "sha256:7a7d881137bf348cde969d40d09d9659e9c60b545c30c0e6ae0f0f0b22c2e29f",
+  "libex_bashkit-v0.1.0-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:e7e4329db39902f4c079c1af57bd2cd5b1d7ddbad5cf40753e7406d16e115863",
+}
